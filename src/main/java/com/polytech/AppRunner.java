d3f3c5979b7fbcaec308c230b1d6c8ee2f71fe84
BarbDev <tanguy.poret@etu.univ-amu.fr>
2018-04-13 10:31:51 +0200

    database

--- src/main/java/com/polytech/AppRunner.java
+++ src/main/java/com/polytech/AppRunner.java
@@ -2,11 +2,13 @@ package com.polytech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AppRunner {
 
     public static void main(String[] args) {
+        //String test = new BCryptPasswordEncoder().encode("a") ;
         SpringApplication.run(AppRunner.class, args);
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,3 +2,19 @@ CREATE TABLE STORY (
   ID      INT IDENTITY,
   CONTENT VARCHAR(144)
 );
+create table users(
+	username varchar_ignorecase(50) not null primary key,
+	password varchar_ignorecase(250) not null,
+	enabled boolean not null
+);
+
+create table authorities (
+	username varchar_ignorecase(50) not null,
+	authority varchar_ignorecase(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
+
+-- create user
+INSERT INTO users VALUES('a','$2a$10$bT.U.ZEJOBhQ1.dAnifAk.wSTPbTjLnBpt5Q5zXN2qZlw85InW.Fu',true);
+INSERT INTO authorities VALUES('a','ADMIN');
